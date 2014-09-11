d8b2a73c4c7cea0e47a7863ca94ed8529bedfdb1
AQ <aq@okaq.com>
2014-09-11 09:59:15 -0400

    First web render

--- web.go
+++ web.go
@@ -5,8 +5,24 @@ package main
 
 import (
 	"fmt"
+    "log"
+    "net/http"
 )
+
+const (
+    HTML = "joruri.html"
+)
+
+func JoruriServer(w http.ResponseWriter, req *http.Request) {
+    fmt.Println(req)
+    http.ServeFile(w, req, HTML)
+}
 
 func main() {
 	fmt.Println("okaq.jouri starting...")
+    http.HandleFunc("/", JoruriServer)
+    err := http.ListenAndServe(":8008", nil)
+    if err != nil {
+        log.Fatal("ListenAndServe: " , err)
+    }
 }
