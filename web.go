ac1388ed51af90db3ca1db11ec70479731384e3e
AQ <aq@okaq.com>
2014-10-06 11:50:56 -0400

    Server side json endpoint

--- web.go
+++ web.go
@@ -19,9 +19,17 @@ func JoruriServer(w http.ResponseWriter, req *http.Request) {
 	http.ServeFile(w, req, HTML)
 }
 
+func OpenServer(w http.ResponseWriter, req *http.Request) {
+    fmt.Println(req)
+    // time simulate network lag
+    hi := []byte("hi there from AServer")
+    w.Write(hi)
+}
+
 func main() {
 	fmt.Printf("okaq.jouri starting on port %s\n", PORT)
 	http.HandleFunc("/", JoruriServer)
+    http.HandleFunc("/aa", OpenServer)
 	err := http.ListenAndServe(PORT, nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
