c2be6cf64144ce19ede980a4b80881184a52b1dc
AQ <aq@okaq.com>
2014-10-07 19:53:40 -0400

    Conn color coded state changes

--- web.go
+++ web.go
@@ -7,7 +7,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-    // "time"
+    "time"
 )
 
 const (
@@ -29,14 +29,14 @@ func OpenServer(w http.ResponseWriter, req *http.Request) {
     // time simulate network lag
     // hi := []byte("hi there from AServer\n")
     // w.Write(hi)
-    /*
+    /**/
     for i := 0; i < 8; i++ {
         time.Sleep(1 * time.Second)
         mess := []byte(fmt.Sprintf("this is message number: %d.", i))
         w.Write(mess)
     }
-    */
-    w.Header().Set("Content-Length", "131072")
+    /**/
+    // w.Header().Set("Content-Length", "131072")
     // w.Header().Set("Doobie-Doo", "ItsaDoob!")
     w.WriteHeader(200)
     w.Write(big)
@@ -44,7 +44,7 @@ func OpenServer(w http.ResponseWriter, req *http.Request) {
 
 func main() {
 	fmt.Printf("okaq.jouri starting on port %s\n", PORT)
-    big = make([]byte, 1024*128)
+    big = make([]byte, 255)
     for i := 0; i < len(big); i++ {
         big[i] = byte(i % 255)
     }
