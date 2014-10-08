3c082235b34ee3fe9556fefde7c6ac22ce2e405d
AQ <aq@okaq.com>
2014-10-08 09:07:40 -0400

    Save handler for decoding XHR json bitmaps

--- web.go
+++ web.go
@@ -4,10 +4,11 @@
 package main
 
 import (
+    "encoding/json"
 	"fmt"
 	"log"
 	"net/http"
-    "time"
+    // "time"
 )
 
 const (
@@ -29,18 +30,56 @@ func OpenServer(w http.ResponseWriter, req *http.Request) {
     // time simulate network lag
     // hi := []byte("hi there from AServer\n")
     // w.Write(hi)
-    /**/
+    /*
     for i := 0; i < 8; i++ {
         time.Sleep(1 * time.Second)
         mess := []byte(fmt.Sprintf("this is message number: %d.", i))
         w.Write(mess)
     }
-    /**/
+    */
     // w.Header().Set("Content-Length", "131072")
     // w.Header().Set("Doobie-Doo", "ItsaDoob!")
     w.WriteHeader(200)
     w.Write(big)
 }
+
+type Bitmap struct {
+    Grayscales int
+    Grids []int
+}
+
+func SaveHandler(w http.ResponseWriter, req *http.Request) {
+    /*
+    var b []byte
+    var err error
+    _, err = req.Body.Read(b)
+    if err != nil {
+        fmt.Println(err)
+    }
+    fmt.Println(string(b))
+    err = req.ParseForm()
+    if err != nil {
+        fmt.Println(err)
+    }
+    fmt.Println(req.Form)
+    for k := range req.Header {
+        fmt.Println(k)
+        fmt.Println(req.Form[k])
+    }
+    w.Write([]byte("json file stored"))
+    */
+    dec := json.NewDecoder(req.Body)
+    var bm Bitmap
+    err := dec.Decode(&bm)
+    if err != nil {
+        fmt.Println(err)
+    }
+    fmt.Println(bm)
+    w.Write([]byte("json file decoded and stored"))
+}
+
+// view archive
+// on init, pre load data filenames
 
 func main() {
 	fmt.Printf("okaq.jouri starting on port %s\n", PORT)
@@ -50,6 +89,7 @@ func main() {
     }
 	http.HandleFunc("/", JoruriServer)
     http.HandleFunc("/aa", OpenServer)
+    http.HandleFunc("/ab", SaveHandler)
 	err := http.ListenAndServe(PORT, nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
