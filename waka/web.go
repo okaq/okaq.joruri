c947c20c60ef7f2ed883fc7e77c6a43e235205e4
AQ <aq@okaq.com>
2014-12-15 09:09:40 -0500

    Waka image sampling init

---
+++ waka/web.go
@@ -0,0 +1,29 @@
+// okaq.joruri web server
+// waka image render and sampling tool
+// AQ <aq@okaq.com>
+// 2014-12-16
+package main
+
+import (
+    "fmt"
+    "net/http"
+)
+
+const (
+    HTML = "aomi.html"
+    PORT = ":8080"
+)
+
+func WakaServer(w http.ResponseWriter, req *http.Request) {
+    fmt.Println(req)
+    http.ServeFile(w, req, HTML)
+}
+
+func main() {
+    fmt.Printf("okaq.joruri waka starting on port%s\n", PORT)
+    http.HandleFunc("/waka", WakaServer)
+    err := http.ListenAndServe(PORT, nil)
+    if err != nil {
+        fmt.Println(err)
+    }
+}
