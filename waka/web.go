4533129d4da79c1a6a1c0da78afcfea06c68ccd4
AQ <aq@okaq.com>
2014-12-17 17:15:54 -0500

    Web fonts golang file server

--- waka/web.go
+++ waka/web.go
@@ -23,6 +23,8 @@ func WakaServer(w http.ResponseWriter, req *http.Request) {
 func main() {
     fmt.Printf("okaq.joruri waka starting on port%s\n", PORT)
     http.HandleFunc("/waka", WakaServer)
+    http.Handle("/fonts", http.FileServer(http.Dir("/home/ahmad/Documents/gira/okaq.joruri/fonts")))
+    // http.Handle("/", http.FileServer(http.Dir("/home/ahmad/Documents/gira/okaq.joruri")))
     err := http.ListenAndServe(PORT, nil)
     if err != nil {
         fmt.Println(err)
