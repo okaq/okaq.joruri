4a3954fe4575395a4a7b50a7d9e711e3981725c4
AQ <aq@okaq.com>
2014-12-19 10:13:39 -0500

    Xhr connection and web server handler

--- waka/web.go
+++ waka/web.go
@@ -6,7 +6,9 @@ package main
 
 import (
     "fmt"
+    "math/rand"
     "net/http"
+    "time"
 )
 
 const (
@@ -14,6 +16,11 @@ const (
     HTML = "bome.html"
     PORT = ":8080"
     FONT = "/home/ahmad/Documents/gira/okaq.joruri/fonts/Kalocsai_Flowers-webfont.woff"
+)
+
+var (
+    Source rand.Source
+    Rng *rand.Rand
 )
 
 func WakaServer(w http.ResponseWriter, req *http.Request) {
@@ -28,12 +35,26 @@ func FontServer(w http.ResponseWriter, req *http.Request) {
     http.ServeFile(w, req, FONT)
 }
 
+// handshake - xhr connection test handler
+func ShakeServer(w http.ResponseWriter, req *http.Request) {
+    fmt.Println(req)
+    w.Header().Set("Content-Type", "application/json")
+    type Shake struct {
+        Id, Now string
+    }
+    s0 := new(Shake)
+    fmt.Println(s0) 
+}
+
 func main() {
+    now := time.Now()
     fmt.Printf("okaq.joruri waka starting on port%s\n", PORT)
+    fmt.Printf("Started at: %s.\n", now.Format(time.RFC1123Z))
     http.HandleFunc("/waka", WakaServer)
     http.HandleFunc("/fonts", FontServer)
     // http.Handle("/fonts", http.FileServer(http.Dir("/home/ahmad/Documents/gira/okaq.joruri/fonts")))
     // http.Handle("/", http.FileServer(http.Dir("/home/ahmad/Documents/gira/okaq.joruri")))
+    http.HandleFunc("/shake", ShakeServer)
     err := http.ListenAndServe(PORT, nil)
     if err != nil {
         fmt.Println(err)
