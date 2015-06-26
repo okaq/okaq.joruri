1897ac01bafa0c2591b6741437a9f85c03d43943
AQ <aq@okaq.com>
2015-06-26 08:42:56 -0400

    Bufio stdin reader for CLI input

--- shima/aoni.go
+++ shima/aoni.go
@@ -2,8 +2,11 @@
 package main
 
 import (
+    "bufio"
     "fmt"
+    "os"
     "net/http"
+    "time"
 )
 
 const (
@@ -16,8 +19,24 @@ func BoniHandler(w http.ResponseWriter, req *http.Request) {
     http.ServeFile(w, req, BONI)
 }
 
+func motd() {
+    fmt.Println("The joy of a summer sunrise glows within the soul for eternity! - Emerson (1838)")
+    r0 := bufio.NewReader(os.Stdin)
+    fmt.Println("Who are you? Please type in your name and press <Enter>")
+    t0, err := r0.ReadString('\n')
+    if err != nil {
+        fmt.Println(err)
+    }
+    // strip trailing newline
+    t0 = t0[:len(t0)-1]
+    fmt.Printf("Greetings %s! Welcome to the okaq.joruri bitmap importer\n", t0)
+}
+
 func main() {
-    fmt.Printf("okaq.joruri shima aoni web app started on port %s\n", PORT)
+    now := time.Now()
+    fmt.Printf("okaq.joruri shima aoni web app running on port %s\n", PORT)
+    fmt.Printf("Started at: %s\n", now.Format(time.RFC1123Z))
+    motd()
     http.HandleFunc("/boni", BoniHandler)
     err := http.ListenAndServe(PORT, nil)
     if err != nil {
