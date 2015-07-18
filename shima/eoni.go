7556692ab17aaf5a0f80e8df9747e1f9723ec448
AQ <aq@okaq.com>
2015-07-18 08:53:15 -0400

    New file foni.html. Bitmap import and sample

---
+++ shima/eoni.go
@@ -0,0 +1,25 @@
+// okaq.joruri web server
+package main
+
+import (
+    "fmt"
+    "net/http"
+)
+
+const (
+    FONI = "foni.html"
+    PORT = ":8008"
+)
+
+func FoniHandler(w http.ResponseWriter, req *http.Request) {
+    fmt.Println(req)
+    http.ServeFile(w, req, FONI)
+}
+
+func main() {
+    http.HandleFunc("/", FoniHandler)
+    err := http.ListenAndServe(PORT, nil)
+    if err != nil {
+        fmt.Println(err)
+    }
+}
