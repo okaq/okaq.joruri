15ab6967c12afffb2b9257ad4455df94faa0d979
AQ <aq@okaq.com>
2016-01-09 20:41:35 -0500

    JSON post and server decode

--- qian/vian.go
+++ qian/vian.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+    "encoding/json"
     "fmt"
     "net/http"
 )
@@ -9,12 +10,27 @@ const (
     INDEX = "eian.html"
 )
 
+type User struct {
+    A int64 // browser rand
+    B int64 // server rand
+    C int64 // nano time stamp
+}
+
 func UserHandler(w http.ResponseWriter, r *http.Request) {
     fmt.Println(r)
-    w.Write([]byte("ok fetch!"))
+    // w.Write([]byte("ok fetch!"))
     // marshal request json
+    var d0 []int64
+    dec := json.NewDecoder(r.Body)
+    err := dec.Decode(&d0)
+    if err != nil {
+        fmt.Println(err)
+    }
+    fmt.Println(d0)
+    w.Write([]byte("ok json fetch!"))
     // generate server random, timestamp, hash
     // send to browser
+    // enc := json.NewEncoder(w)
 }
 
 func VianHandler(w http.ResponseWriter, r *http.Request) {
