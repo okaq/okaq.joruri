9f11131c6ac2bb304082c1ac7a8e9a1ca5a7795e
AQ <aq@okaq.com>
2016-01-10 09:28:42 -0500

    User struct and next function

--- qian/vian.go
+++ qian/vian.go
@@ -3,17 +3,41 @@ package main
 import (
     "encoding/json"
     "fmt"
+    "math/rand"
     "net/http"
+    "time"
 )
 
 const (
     INDEX = "eian.html"
 )
 
+var (
+    U *User
+)
+
 type User struct {
     A int64 // browser rand
     B int64 // server rand
     C int64 // nano time stamp
+    R *rand.Rand
+    S rand.Source
+    T int64 // seed from time now
+}
+
+func NewUser() *User {
+    // init rand
+    u := new(User)
+    u.T = time.Now().UnixNano()
+    u.S = rand.NewSource(u.T)
+    u.R = rand.New(u.S)
+    return u
+}
+
+func (u *User) Next(a int64) {
+    u.A = a
+    u.B = u.R.Int63()
+    u.C = time.Now().UnixNano()
 }
 
 func UserHandler(w http.ResponseWriter, r *http.Request) {
@@ -27,6 +51,9 @@ func UserHandler(w http.ResponseWriter, r *http.Request) {
         fmt.Println(err)
     }
     fmt.Println(d0)
+    // user data
+    U.Next(d0[0])
+    fmt.Println(U.B,U.C)
     w.Write([]byte("ok json fetch!"))
     // generate server random, timestamp, hash
     // send to browser
@@ -40,6 +67,7 @@ func VianHandler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
     fmt.Println("okaq joruri bitmap draw tool started on localhost:8008")
+    U = NewUser()
     http.HandleFunc("/", VianHandler)
     http.HandleFunc("/user", UserHandler)
     http.ListenAndServe(":8008", nil)
