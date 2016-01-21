1fc47bcd2aa17c093adc9193a18f6d4b337574ab
AQ <aq@okaq.com>
2016-01-20 21:14:12 -0500

    Pre-populate dir lists

--- qian/noto/aion.go
+++ qian/noto/aion.go
@@ -16,8 +16,26 @@ func NotoHandler(w http.ResponseWriter, r *http.Request) {
     http.ServeFile(w,r,INDEX)
 }
 
+func NetoHandler(w http.ResponseWriter, r *http.Request) {
+    fmt.Println(r)
+    w.Write([]byte("ok neto"))
+}
+
+func NitoHandler(w http.ResponseWriter, r *http.Request) {
+    fmt.Println(r)
+    w.Write([]byte("ok nito"))
+}
+
+
+func Pop() {
+    // pre populate dir lists
+}
+
 func main() {
     fmt.Println("okaq joruri qian noto thumb render test is on...")
+    Pop()
     http.HandleFunc("/", NotoHandler)
+    http.HandleFunc("/neto", NetoHandler)
+    http.HandleFunc("/nito", NitoHandler)
     http.ListenAndServe(":8000", nil)
 }
