798bf9dcfd73b098ca4316b51d66c089df077273
AQ <aq@okaq.com>
2016-01-21 09:30:00 -0500

    Paths dir list generator

--- qian/noto/aion.go
+++ qian/noto/aion.go
@@ -4,11 +4,17 @@ package main
 
 import (
     "fmt"
+    "io/ioutil"
     "net/http"
 )
 
 const (
     INDEX = "zion.html"
+)
+
+var (
+    Neto []string
+    Nito []string
 )
 
 func NotoHandler(w http.ResponseWriter, r *http.Request) {
@@ -29,7 +35,27 @@ func NitoHandler(w http.ResponseWriter, r *http.Request) {
 
 func Pop() {
     // pre populate dir lists
+    Neto = Paths("../neto")
+    Nito = Paths("../nito")
+    fmt.Println(Neto, Nito)
 }
+
+func Paths(d0 string) []string {
+    f0, err := ioutil.ReadDir(d0)
+    if err != nil {
+        fmt.Println(err)
+    }
+    s0 := make([]string, 1)
+    s0[0] = "files"
+    for _, f1 := range f0 {
+        if f1.IsDir() {
+            continue
+        }
+        s0 = append(s0, f1.Name())
+    }
+    return s0[1:]
+}
+
 
 func main() {
     fmt.Println("okaq joruri qian noto thumb render test is on...")
