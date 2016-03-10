6c56f276f7e1a5f8c8b5e3ad9b6ca7fdaf154e3f
AQ <aq@okaq.com>
2016-03-10 08:42:57 -0500

    New tool, unki. Texture synth

---
+++ unki/znki.go
@@ -0,0 +1,26 @@
+// golang web server
+// unki - patch based texture synth
+// okaq joruri bitmap draw tools
+// AQ <aq@okaq.com>
+// 2016-03-12
+package main
+
+import (
+    "fmt"
+    "net/http"
+)
+
+const (
+    ANKI = "anki.html"
+)
+
+func AnkiHandler(w http.ResponseWriter, r *http.Request) {
+    fmt.Println(r)
+    http.ServeFile(w, r, ANKI)
+}
+
+func main() {
+    fmt.Println("starting unki web on localhost:8800")
+    http.HandleFunc("/", AnkiHandler)
+    http.ListenAndServe(":8800", nil)
+}
