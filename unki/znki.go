21fe4a0b996d9659003c06986677a3a75c6124f0
AQ <aq@okaq.com>
2016-03-20 16:54:10 -0400

    Open net conn

--- unki/znki.go
+++ unki/znki.go
@@ -19,8 +19,14 @@ func AnkiHandler(w http.ResponseWriter, r *http.Request) {
     http.ServeFile(w, r, ANKI)
 }
 
+func OpenHandler(w http.ResponseWriter, r *http.Request) {
+    fmt.Println(r)
+    w.Write([]byte("ok bitmaps!"))
+}
+
 func main() {
     fmt.Println("starting unki web on localhost:8800")
     http.HandleFunc("/", AnkiHandler)
+    http.HandleFunc("/open", OpenHandler)
     http.ListenAndServe(":8800", nil)
 }
