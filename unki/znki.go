0a54edd61f89579830b86814a1eac4798a7aa90b
AQ <aq@okaq.com>
2016-03-21 09:09:17 -0400

    Cache server side sample image file paths

--- unki/znki.go
+++ unki/znki.go
@@ -7,11 +7,19 @@ package main
 
 import (
     "fmt"
+    "io/ioutil"
     "net/http"
 )
 
 const (
     ANKI = "anki.html"
+    // sample and save dirs
+    SAMPLE = "sample/"
+    SAVE = "save/"
+)
+
+var (
+    SamplePaths []string
 )
 
 func AnkiHandler(w http.ResponseWriter, r *http.Request) {
@@ -22,10 +30,31 @@ func AnkiHandler(w http.ResponseWriter, r *http.Request) {
 func OpenHandler(w http.ResponseWriter, r *http.Request) {
     fmt.Println(r)
     w.Write([]byte("ok bitmaps!"))
+    // ioutil.Readdir returns []os.FileInfo
+    // use len() to get dir size
+}
+
+func Paths() []string {
+    f0, err := ioutil.ReadDir(SAMPLE)
+    if err != nil {
+        fmt.Println(err)
+    }
+    s0 := make([]string, len(f0))
+    for i0, f1 := range f0 {
+        if f1.IsDir() {
+            // dirs should not contain sub dirs
+            continue
+        }
+        // s0 = append(s0, f1.Name())
+        s0[i0] = f1.Name()
+    }
+    return s0
 }
 
 func main() {
     fmt.Println("starting unki web on localhost:8800")
+    SamplePaths = Paths()
+    fmt.Println(SamplePaths, len(SamplePaths))
     http.HandleFunc("/", AnkiHandler)
     http.HandleFunc("/open", OpenHandler)
     http.ListenAndServe(":8800", nil)
